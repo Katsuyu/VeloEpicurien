d3ec488aac189761067b0cdc430d1d0b949b5633
LeChatErrant <guillaume.hector@epitech.eu>
2020-09-22 19:46:35 +0200

    feat: villeChoisie

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/ping', (req, res) => res.send('pong'));
+router.get('/heartbeat', (req, res) => res.send({ villeChoisie: 'Paris' }));
 
 export default router;
