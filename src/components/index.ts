7aeabc961b854bc3b2140680177e166f6762e652
LeChatErrant <guillaume.hector@epitech.eu>
2020-11-04 18:14:40 +0100

    misc: updated to match last version

--- src/components/index.ts
+++ src/components/index.ts
@@ -28,14 +28,14 @@ async function connectMongo() {
 const router = express.Router();
 
 router.get('/ping', (req, res) => res.send('pong'));
-router.get('/heartbeat', (req, res) => res.send({ villeChoisie: 'Paris' }));
+router.get('/heartbeat', (req, res) => res.send({ villeChoisie: 'Paris XVIIIème' }));
 
 router.get('/extracted_data', async (req, res) => {
   await connectMongo();
 
   res.send({
     nbRestaurants: await mongo.db('veloepicurien').collection('restaurants').countDocuments(),
-    nbSegments: await neo.run('MATCH ()-[segment:Route]-() RETURN count(segment)'),
+    nbSegments: (await neo.run('MATCH ()-[segment:Route]-() RETURN segment')).records.length,
   });
 });
 
@@ -51,7 +51,9 @@ router.get('/transformed_data', async (req, res) => {
     restaurants: restaurantsStats.map((stat) => ({
       [stat._id]: stat.count,
     })),
-    longueurCyclable: 0.0,
+    // Since every nodes are connected in a bidirectional way, we divide the number of Routes by 2
+    // Real distance will be used later, since openStreetMap API doesn't provide this information (even if they documented that they would)
+    longueurCyclable: (await neo.run('MATCH ()-[segment:Route]-() RETURN segment')).records.length / 2,
   });
 });
 
